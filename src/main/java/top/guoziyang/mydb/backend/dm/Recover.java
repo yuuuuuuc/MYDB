50fe6d16fa3be6d36232ff2cc0370acf4f17bd40
yuuc <yuuuuc@126.com>
2025-03-29 10:59:42 +0800

    添加注释

--- src/main/java/top/guoziyang/mydb/backend/dm/Recover.java
+++ src/main/java/top/guoziyang/mydb/backend/dm/Recover.java
@@ -19,6 +19,7 @@ import top.guoziyang.mydb.backend.tm.TransactionManager;
 import top.guoziyang.mydb.backend.utils.Panic;
 import top.guoziyang.mydb.backend.utils.Parser;
 
+//Recover 恢复机制
 public class Recover {
 
     private static final byte LOG_TYPE_INSERT = 0;
@@ -27,13 +28,15 @@ public class Recover {
     private static final int REDO = 0;
     private static final int UNDO = 1;
 
+
+    //插入日志
     static class InsertLogInfo {
         long xid;
         int pgno;
         short offset;
         byte[] raw;
     }
-
+    //更新日志
     static class UpdateLogInfo {
         long xid;
         int pgno;
@@ -45,12 +48,13 @@ public class Recover {
     public static void recover(TransactionManager tm, Logger lg, PageCache pc) {
         System.out.println("Recovering...");
 
-        lg.rewind();
-        int maxPgno = 0;
+        lg.rewind();//重置日志文件指针
+        int maxPgno = 0;//最大页号
         while(true) {
             byte[] log = lg.next();
             if(log == null) break;
             int pgno;
+            //如果是插入日志
             if(isInsertLog(log)) {
                 InsertLogInfo li = parseInsertLog(log);
                 pgno = li.pgno;
@@ -80,17 +84,20 @@ public class Recover {
     private static void redoTranscations(TransactionManager tm, Logger lg, PageCache pc) {
         lg.rewind();
         while(true) {
-            byte[] log = lg.next();
-            if(log == null) break;
+            byte[] log = lg.next();//读取下一条日志
+            if(log == null) break;//为0 ，跳出循环
+
             if(isInsertLog(log)) {
                 InsertLogInfo li = parseInsertLog(log);
                 long xid = li.xid;
+                //如果事务已经提交，进行删除重做
                 if(!tm.isActive(xid)) {
                     doInsertLog(pc, log, REDO);
                 }
             } else {
                 UpdateLogInfo xi = parseUpdateLog(log);
                 long xid = xi.xid;
+                //如果事务已经提交，进行更新重做
                 if(!tm.isActive(xid)) {
                     doUpdateLog(pc, log, REDO);
                 }
@@ -99,21 +106,25 @@ public class Recover {
     }
 
     private static void undoTranscations(TransactionManager tm, Logger lg, PageCache pc) {
+        //创建一个存储日志的映射，键为事务ID，值为日志列表
         Map<Long, List<byte[]>> logCache = new HashMap<>();
         lg.rewind();
         while(true) {
             byte[] log = lg.next();
             if(log == null) break;
             if(isInsertLog(log)) {
-                InsertLogInfo li = parseInsertLog(log);
-                long xid = li.xid;
+                InsertLogInfo li = parseInsertLog(log);//解析插入日志
+                long xid = li.xid;//事务ID
+                //判断是否为活动事务
                 if(tm.isActive(xid)) {
                     if(!logCache.containsKey(xid)) {
+                        //如果当前事务任然活跃，将日志记录添加到对应的日志列表中
                         logCache.put(xid, new ArrayList<>());
                     }
                     logCache.get(xid).add(log);
                 }
             } else {
+                //解析更新日志
                 UpdateLogInfo xi = parseUpdateLog(log);
                 long xid = xi.xid;
                 if(tm.isActive(xid)) {
@@ -136,10 +147,13 @@ public class Recover {
                     doUpdateLog(pc, log, UNDO);
                 }
             }
+            //中止当前事务
             tm.abort(entry.getKey());
         }
     }
 
+
+    //判断是否为插入日志
     private static boolean isInsertLog(byte[] log) {
         return log[0] == LOG_TYPE_INSERT;
     }
@@ -173,28 +187,33 @@ public class Recover {
         return li;
     }
 
+    //更新功能
     private static void doUpdateLog(PageCache pc, byte[] log, int flag) {
         int pgno;
         short offset;
         byte[] raw;
+        //如果是重做操作
         if(flag == REDO) {
             UpdateLogInfo xi = parseUpdateLog(log);
             pgno = xi.pgno;
             offset = xi.offset;
-            raw = xi.newRaw;
+            raw = xi.newRaw;//设置为新数据
         } else {
             UpdateLogInfo xi = parseUpdateLog(log);
             pgno = xi.pgno;
             offset = xi.offset;
-            raw = xi.oldRaw;
+            raw = xi.oldRaw;//设置为旧数据
         }
         Page pg = null;
+        //尝试从页面缓存中获取指定页码的页面
         try {
             pg = pc.getPage(pgno);
         } catch (Exception e) {
             Panic.panic(e);
         }
+        //修改页面
         try {
+            //在指定的页面和偏移量处插入解析出的数据，
             PageX.recoverUpdate(pg, raw, offset);
         } finally {
             pg.release();
@@ -223,21 +242,24 @@ public class Recover {
         return li;
     }
 
+    //插入功能
     private static void doInsertLog(PageCache pc, byte[] log, int flag) {
         InsertLogInfo li = parseInsertLog(log);
         Page pg = null;
         try {
-            pg = pc.getPage(li.pgno);
+            pg = pc.getPage(li.pgno);//从页面缓存中获取指定页面
         } catch(Exception e) {
             Panic.panic(e);
         }
         try {
+            //如果是undo操作，将数据项标记为无效
             if(flag == UNDO) {
                 DataItem.setDataItemRawInvalid(li.raw);
             }
+            //如果是redo操作，在指定页面和偏移处插入数据
             PageX.recoverInsert(pg, li.raw, li.offset);
         } finally {
-            pg.release();
+            pg.release();//释放页面
         }
     }
 }
